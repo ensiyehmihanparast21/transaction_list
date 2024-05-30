242a2808d35c3d2c568cf98435134da17754f46a
Mihanparast <Mihanparast.Ensieh@HyperFamili.com>
2024-05-30 11:50:28 +0330

    me

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://gatewayapi.eniac-tech.com/api/v1/transaction_list'
+  baseUrl: 'https://gatewayapi.eniac-tech.com/api/v1/transaction_list',
+  apiUrl: 'https://gatewayapi.eniac-tech.com/api/v1/transaction_list',
+  domain: 'example.com',
+  serverUrl: 'https://example.com'
 }
